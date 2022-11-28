f36d31e0caed3e0a2ac35ee45329ec020762e69c
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-28 13:28:43 +0300

    created departments page

---
+++ src/views/department.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Departments page</h1>
+</template>
+
+<script>
+export default {
+    name: 'Department',
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
