ddd71822b3e02ecdcd5dd004ff413b087ce803bd
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-05 08:48:24 +0300

    created Aboutview page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,40 @@
 <template>
+  <img :src="img" style="height:700px;"/>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About the Hospital</h1>
+  <i class="fa-solid fa-person-dots-from-line fa-3x"></i> <h2>Diagnosis</h2>
+  <p>We have a team that is comnited to make sure that our patients are confortable in their visit to our facility by ensuring that they are treated to the correct illness by gettin the correct diagnosis.</p>
   </div>
 </template>
 <script>
   export default{
     name: 'AboutView',
     components:{
-
+      img: './img/aboutimage.png',
     }
   }
 </script>
+<style>
+img{
+  margin-bottom: 20px;
+}
+.about{
+  margin: 0px
+}
+  h1{
+    text-align: center;
+    color:#212529;
+    margin-left: 5px;
+  }
+  i{
+    float: center;
+    margin-left: 5px;
+    margin-right:0px;
+  }
+  h2{
+   font-size:18px 
+  }
+  p{
+    text-align: center;
+  }
+</style>
