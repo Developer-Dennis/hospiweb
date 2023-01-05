5ae099037599381444e3d3145be1d5c11501fdb8
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-05 15:05:58 +0300

    deleted service page routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-import Services from   '../views/services.vue'
 import Work from  '../views/work-with-us.vue'
 import Contact from '../views/contact.vue'
 import Department from '../views/department.vue'
@@ -18,11 +17,7 @@ const routes = [
     name: 'about',
     component: AboutView
   },
-  {
-    path: '/services',
-    name: 'services',
-    component: Services
-  },
+  
   {
     path: '/work-with-us',
     name: 'work-with-us',
