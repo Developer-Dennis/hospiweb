221cbf92b9b7a72a4a63cee3c519e5c3530c5855
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-05 08:49:02 +0300

    created department  page

--- src/views/department.vue
+++ src/views/department.vue
@@ -1,16 +1,32 @@
 <template>
-    <h1>Departments page</h1>
+  <div class="departments">
+    <h1>Neurology</h1>
+    <img :src="img" style="height:300px;"/>
+  </div>
 </template>
-
+    
 <script>
 export default {
     name: 'Department',
+    data(){
+      return{                                    
+        img: './img/neuralogy.jpg',
+      }
+    },
     components:{
-        
+     
     }
 }
 </script>
 
 <style>
-
+    .departments{
+      padding-right : 50px;
+      background-color:#f2f2f2;
+    }
+    .departments img{
+      display: flex;
+      align-items: right;
+      display: block;
+    }
 </style>
