ab1ad2a4583b67cafa14af8598f759ba90e3732f
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-06 16:23:51 +0300

    added department to the page

--- src/views/department.vue
+++ src/views/department.vue
@@ -5,16 +5,15 @@
     <button @click="toggleImage">Show more..</button>
 
     <h1>Cardiology</h1>
-    <img :src="cardiology"  v-if='showImage' style="height:300px;"/>
-    <button @click="toggleImage">Show more...</button>
+    <img :src="cardiology"  v-if='showImage1' style="height:300px;"/>
+    <button @click="toggleImage1">Show more...</button>
 
     <h1>Orthopaedic</h1>
     <img :src="orthopaedic"  v-if='showImage' style="height:300px;"/>
-    <button @click="toggleImage">Show more...</button>
+    <button @click="toggleImage1">Show more...</button>
 
     <h1>pediatric</h1>
     <img :src="pediatric"  v-if='showImage' style="height:300px;"/>
-    <p  v-if='showText'>Our pediatric/Children's clinic is to assist your child to have the best of health care so as to live happily. We do have a team of specilized physicians to take care of your child so as to be comfortable in their day to day life.</p>
     <button @click="toggleImage">Show more...</button>
 
     <h1> Ophalmology</h1>
@@ -55,6 +54,34 @@
     <h1>Critical Care</h1>
     <img :src="icu"  v-if='showImage' style="height:300px;"/>
     <button @click="toggleImage">Show more...</button>
+
+    <h1>Ear Nose &Throat care </h1>
+    <img :src="ent"  v-if='showImage' style="height:300px;"/>
+    <button @click="toggleImage">Show more...</button>
+
+    <h1>Gynaecology & Reproductive Health Care </h1>
+    <img :src="gynaecology"  v-if='showImage' style="height:300px;"/>
+    <button @click="toggleImage">Show more...</button>
+
+
+    <h1>Physiotherapy Services </h1>
+    <img :src="physiotherapy"  v-if='showImage' style="height:300px;"/>
+    <button @click="toggleImage">Show more...</button>
+
+
+    <h1>Psychiatry Services ( Mental Health) </h1>
+    <img :src="Mentalhealth"  v-if='showImage' style="height:300px;"/>
+    <button @click="toggleImage">Show more...</button>
+
+
+    <h1>General Surgery </h1>
+    <img :src="generalsurgery"  v-if='showImage' style="height:300px;"/>
+    <button @click="toggleImage">Show more...</button>
+
+
+    <h1>Funeral Home servies</h1>
+    <img :src="departments2"  v-if='showImage' style="height:300px;"/>
+    <button @click="toggleImage">Show more...</button>
   </div>
 
 
@@ -68,7 +95,7 @@ export default {
     name: 'Department',
     data(){
       return{    
-        showImage: true,                                
+        showImage: true,                              
         neurology: './img/neuralogy.jpg',
         cardiology:  './img/cardiology.jpg',
         orthopaedic:  './img/orthopaedic',
@@ -82,12 +109,17 @@ export default {
         oncology:       './img/oncology.png',
         family:         './img/family.png',
         icu:             './img/icu.png',
+        ent:           './img/ent.jpg',
+        gynaecology:    './img/gynaecology.jpg',
+        physiotherapy:    './img/physiotherapy.png',
+        Mentalhealth:    './img/Mental-health.jpg',
+        generalsurgery:    './img/generalsurgery.jpg',
+        departments2:     './img/departments2.jpg',
       }
     },
     methods: {
         toggleImage: function () {
             this.showImage = !this.showImage;
-            this.showText = !this.showText;
         },
       
     },
@@ -106,5 +138,11 @@ export default {
       display: flex;
       align-items: right;
       display: block;
+    }
+    .departments h1{
+      text-align: left;
+    }
+    button{
+      width: 100%;
     }
 </style>
